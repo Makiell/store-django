af08857a2af914555da2aeae64a7f926f1a62c84
Makiell <123373280+Makiell@users.noreply.github.com>
2023-06-24 18:31:08 +0600

    store payment + email verification + CBV

--- products/models.py
+++ products/models.py
@@ -1,13 +1,20 @@
+import stripe
+from django.conf import settings
 from django.db import models
+
 from users.models import User
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
 # Create your models here.
 
 # модели = таблицы
 
+
 class ProductCategory(models.Model):
-    name = models.CharField(max_length=128, unique = True)
+    name = models.CharField(max_length=128, unique=True)
     description = models.TextField(null=True, blank=True)
-    
+
     class Meta:
         verbose_name = 'category'
         verbose_name_plural = 'categories'
@@ -23,6 +30,7 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+    stripe_product_price_id = models.CharField(max_length=128, null=True, blank=True)
 
     class Meta:
         verbose_name = 'product'
@@ -31,16 +39,37 @@ class Product(models.Model):
     def __str__(self):
         return f'Продукт: {self.name} | Категория: {self.category.name}'
 
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        if not self.stripe_product_price_id:
+            stripe_product_price = self.create_stripe_product_price()
+            self.stripe_product_price_id = stripe_product_price['id']
+        super(Product, self).save(force_insert=False, force_update=False, using=None, update_fields=None)
+
+    def create_stripe_product_price(self):
+        stripe_product = stripe.Product.create(name=self.name)
+        stripe_product_price = stripe.Price.create(
+            product=stripe_product['id'], unit_amount=round(self.price*100), currency='rub'
+        )
+        return stripe_product_price
+
 
 class BasketQuerySet(models.QuerySet):
 
     def total_sum(self):
         return sum(basket.sum() for basket in self)
-        
+
     def total_quantity(self):
         return sum(basket.quantity for basket in self)
 
-
+    def stripe_products(self):
+        line_items = []
+        for basket in self:
+            item = {
+                'price': basket.product.stripe_product_price_id,
+                'quantity': basket.quantity
+            }
+            line_items.append(item)
+        return line_items
 
 
 class Basket(models.Model):
@@ -53,6 +82,15 @@ class Basket(models.Model):
 
     def __str__(self):
         return f'Корзина для {self.user.username} | Продукт: {self.product.name}'
-    
+
     def sum(self):
         return self.product.price * self.quantity
+
+    def de_json(self):
+        basket_item = {
+            'product_name': self.product.name,
+            'quantity': self.quantity,
+            'price': float(self.product.price),
+            'sum': float(self.sum()),
+        }
+        return basket_item
--- products/views.py
+++ products/views.py
@@ -1,41 +1,47 @@
-from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator
+from django.shortcuts import HttpResponseRedirect
+from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
 
-from products.models import Product, ProductCategory, Basket
-from users.models import User
+from common.views import TitleMixin
+from products.models import Basket, Product, ProductCategory
+
 # Create your views here.
 
 # функции = контроллеры = фьюхи
 
-def index(request):
-    context = {
-        'title': 'Makiell Store',
-    }
-    return render(request, 'products/index.html', context)
 
-def products(request, category_id=None, page_number=1):
-    # PRODUCTS = Product.objects.all()
-    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+class IndexView(TitleMixin, TemplateView):
+    template_name = 'products/index.html'
+    title = 'Store'
 
-    per_page = 3
-    paginator = Paginator(products, per_page)
-    products_paginator = paginator.page(page_number)
-    
-    # categories = ProductCategory.objects.all()
-    # non_zero_categories = []
-    # for category in categories:
-    #     for product in PRODUCTS:
-    #         if product.category.name == category.name:
-    #             non_zero_categories.append(category)
-    #             break
-        
-    context = {
-        'title': 'Store - Каталог',
-        'categories': ProductCategory.objects.all(),
-        'products': products_paginator
-    }
-    return render(request, 'products/products.html', context)
+
+class ProductListView(TitleMixin, ListView):
+    model = Product
+    template_name = 'products/products.html'
+    paginate_by = 3
+    title = 'Store - Каталог'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id) if category_id else queryset
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context['categories'] = ProductCategory.objects.all()
+
+        # Cache objects
+
+        # categories = cache.get('categories')
+        # if not categories:
+        #     context['categories'] = ProductCategory.objects.all()
+        #     cache.set('categories', context['categories'], 30)
+        # else:
+        #     context['categories'] = categories
+
+        return context
+
 
 @login_required
 def basket_add(request, product_id):
@@ -50,6 +56,7 @@ def basket_add(request, product_id):
         basket.save()
 
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
 
 @login_required
 def basket_remove(request, basket_id):
