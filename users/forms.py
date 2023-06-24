da7bdd6628949a912752e058bc12f3207b48ebe5
Makiell <123373280+Makiell@users.noreply.github.com>
2023-06-24 18:32:31 +0600

    store payment + email verification + CBV

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,12 @@
+from typing import Any
+
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import (AuthenticationForm, UserChangeForm,
+                                       UserCreationForm)
+
 from users.models import User
+from users.tasks import send_email_verification
+
 
 class UserLoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
@@ -11,6 +17,7 @@ class UserLoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ('username', 'password')
+
 
 class UserRegistrationForm(UserCreationForm):
     first_name = forms.CharField(widget=forms.TextInput(attrs={
@@ -30,13 +37,19 @@ class UserRegistrationForm(UserCreationForm):
         model = User
         fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
 
-class UserProfileForm (UserChangeForm):
+    def save(self, commit=True) -> Any:
+        user = super().save(commit=True)
+        send_email_verification.delay(user.id)
+        return user
+
+
+class UserProfileForm(UserChangeForm):
     first_name = forms.CharField(widget=forms.TextInput(attrs={'class': "form-control py-4"}))
     last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4'}))
-    image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'custom-file-label'}), required = False)
+    image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'custom-file-label'}), required=False)
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4', 'readonly': True}))
     email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control py-4', 'readonly': True}))
 
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'image', 'username', 'email' )
+        fields = ('first_name', 'last_name', 'image', 'username', 'email')
--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,16 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from users.views import login, registration, profile, logout
+from users.views import (EmailVerificationView, UserLoginView, UserProfileView,
+                         UserRegistrationView)
 
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', login, name='login'), #.../users/login
-    path('registration/', registration, name='registration'), #.../users/registration
-    path('profile/', profile, name='profile'),
-    path('logout/', logout, name='logout')
+    path('login/', UserLoginView.as_view(), name='login'),  # .../users/login
+    path('registration/', UserRegistrationView.as_view(), name='registration'),  # .../users/registration
+    path('profile/<int:pk>', login_required(UserProfileView.as_view()), name='profile'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('verify/<str:email>/<uuid:code>/', EmailVerificationView.as_view(), name='email_verification'),
 ]
